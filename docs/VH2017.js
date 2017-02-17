601d333b980263556a195d6a3501bd0c950b754e
Visual-HTML <visualhtml@gmail.com>
2017-02-17 23:51:56 +0100

    Update VH2017.js

--- docs/VH2017.js
+++ docs/VH2017.js
@@ -7,6 +7,8 @@ VH2017.document={};
 VH2017.document.body={};
 VH2017.document.body.contentEditable={};
 VH2017.document.body.designMode={};
+VH2017.handlerKey = Date.now();
+VH2017.currentTarget = null;
 
 function InitializeUserAgent(e) {
 	
@@ -36,7 +38,8 @@ function InitializeContent() {
 	for (var i = 0 ; i < _elements.length ; i++) {
 		_elements[i].addEventListener('click', ElementClicked, false);
 		_elements[i].addEventListener('keydown', ReturnPressed, false);
-		_elements[i].style.border = "1px dashed gray";		
+		_elements[i].style.border = "1px dashed gray";
+		_elements[i].setAttribute("data-VH2017-hndk",VH2017.handlerKey);
 		_elements[i].contentEditable = true;
 	}
 	
@@ -92,19 +95,85 @@ function InitializeDesigner_Step1() {
 function ElementClicked(e) {
   e.stopPropagation();
   e.preventDefault();
+   
+  /* look for and attahc un handed child nodes */
+  for (var i = 0 ; i < e.currentTarget.childNodes.length ; i++) {
+	  if (e.currentTarget.childNodes[i].nodeType != 3 && 
+		  e.currentTarget.childNodes[i].getAttribute('data-VH2017-hndk') === null){ 
+		e.currentTarget.childNodes[i].addEventListener('click', ElementClicked, false);
+		e.currentTarget.childNodes[i].addEventListener('keydown', ReturnPressed, false);
+		e.currentTarget.childNodes[i].setAttribute("data-VH2017-hndk",VH2017.handlerKey);
+		e.currentTarget.childNodes[i].style.border = "1px dashed gray";
+	  }
+  }
+
   // a handle on current element is available in e.currentTarget
+  VH2017.currentTarget = e.currentTarget;
+  // now exposed to library code
 }
 function ReturnPressed(e) {
 	
   if (e.which === 13) {
 	  e.preventDefault();
+	  
+	  /* look for and attahc un handed child nodes */
+	  for (var i = 0 ; i < e.currentTarget.childNodes.length ; i++) {
+		  if (e.currentTarget.childNodes[i].nodeType != 3 && 
+			  e.currentTarget.childNodes[i].getAttribute('data-VH2017-hndk') === null){ 
+			e.currentTarget.childNodes[i].addEventListener('click', ElementClicked, false);
+			e.currentTarget.childNodes[i].addEventListener('keydown', ReturnPressed, false);
+			e.currentTarget.childNodes[i].setAttribute("data-VH2017-hndk",VH2017.handlerKey);
+			e.currentTarget.childNodes[i].style.border = "1px dashed gray";
+		  }
+	  }
+	  
 	  var _element = document.createElement("p");
 	  
 		_element.addEventListener('click', ElementClicked, false);
 		_element.addEventListener('keydown', ReturnPressed, false);
+		_element.setAttribute("data-VH2017-hndk",VH2017.handlerKey);
 		_element.style.border = "1px dashed gray";
 		
 		_element.contentEditable = true;
 		document.body.appendChild(_element).focus();
   }
+}
+
+
+
+function ReplaceNode(elt, tagName) {
+	
+	if (elt == null) return;
+	
+	_element = document.createElement(tagName);
+	_element.innerHTML = elt.innerHTML;
+	_element.contentEditable = true;
+	for(var i = 0 ; i < elt.classList.length ; i++) {
+	_element.classList.add(elt.classList[i]);
+	}
+	
+	_element.addEventListener('click', ElementClicked, false);
+	_element.addEventListener('keydown', ReturnPressed, false);
+	_element.setAttribute("data-VH2017-hndk",VH2017.handlerKey);
+	_element.style.border = "1px dashed gray";
+				
+	elt.parentNode.replaceChild(_element,elt);
+	
+	_element.focus();
+	_element.click();
+	
+}
+
+
+
+
+function Align(elt, align) {
+	
+	if (elt == null) return;
+		
+	elt.style.textAlign = align;
+	
+	elt.focus();
+	elt.click();
+	
 }
