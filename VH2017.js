656cc8ce3a1cb179a23a1ca982b56df39bbfc854
Visual-HTML <visualhtml@gmail.com>
2017-02-17 03:30:15 +0100

    Dynamic Code On Starter Code
    Designer code will have a "permanent" menu
    
    Designer code objective is to have a logic in menu panel allowing quite anything:
    1. replace the default, here for instance make the "main" url a parameter in the VH2017 namespace
    2. have a logic in custom menu panels making use of cutom code and custom controls
    Using this technique we will maybe make heavy use of the network but pages will remain light and fast.

--- VH2017.js
+++ VH2017.js
@@ -15,6 +15,30 @@ function InitializeUserAgent(evt) {
 function InitializeDocument(evt) {
 	VH2017.document.body.contentEditable.InitalValue = document.body.contentEditable;
 	VH2017.document.body.designMode.InitialValue = document.designMode;
+	
+	/* if the page is set to design mode, VH don't go further */
+	if (VH2017.document.body.designMode.InitialValue.toLowerCase === "Inherit") return;
+	/* 
+	this mode allow so much that we will use it to open a blank sheet, user will be able to work 
+	in that mode and get the code injected to the VH page
+	*/
+	
+	/* contentEditable processing : in investigation */
+	
+	var xReq = new XMLHttpRequest();
+	xReq.open("GET", "https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/Sources/testmain.html", true); 
+	xReq.timeout = 2000;
+	xReq.ontimeout = function () {   } 
+	xReq.onreadystatechange = function (e) {
+		if (xReq.readyState == 4) {         
+			if (xReq.status = "200") { 
+				document.body.innerHTML += xReq.response;
+				} else {
+				
+				}
+			}
+		}
+	xReq.send(null);
 }
 
 function InitializeContent(evt) {
