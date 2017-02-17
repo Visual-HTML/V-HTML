6e40a95cc8e923c67650f9703c1d80682ffbfe93
Visual-HTML <visualhtml@gmail.com>
2017-02-17 03:31:07 +0100

    Update VH2017.js

--- docs/VH2017.js
+++ docs/VH2017.js
@@ -5,8 +5,8 @@ window.addEventListener('load', InitializeContent, true);
 VH2017 = {};
 VH2017.document={};
 VH2017.document.body={};
-VH2017.document.designMode={};
 VH2017.document.body.contentEditable={};
+VH2017.document.body.designMode={};
 
 function InitializeUserAgent(evt) {
 	
@@ -14,11 +14,35 @@ function InitializeUserAgent(evt) {
 
 function InitializeDocument(evt) {
 	VH2017.document.body.contentEditable.InitalValue = document.body.contentEditable;
-	VH2017.document.designMode.InitialValue = document.designMode;
+	VH2017.document.body.designMode.InitialValue = document.designMode;
+	
+	/* if the page is set to design mode, VH don't go further */
+	if (VH2017.document.body.designMode.InitialValue.toLowerCase === "Inherit") return;
+	/* 
+	this mode allow so much that we will use it to open a blank sheet, user will be able to work 
+	in that mode and get the code injected to the VH page
+	*/
+	
+	/* contentEditable processing : in investigation */
+	
+	var xReq = new XMLHttpRequest();
+	xReq.open("GET", "https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/Sources/testmain.html", true); 
+	xReq.timeout = 2000;
+	xReq.ontimeout = function () {   } 
+	xReq.onreadystatechange = function (e) {
+		if (xReq.readyState == 4) {         
+			if (xReq.status = "200") { 
+				document.body.innerHTML += xReq.response;
+				} else {
+				
+				}
+			}
+		}
+	xReq.send(null);
 }
 
 function InitializeContent(evt) {
-    var _elements = document.querySelectorAll('*');
+    _elements = document.querySelectorAll('*');
 	for (var i = 0 ; i < _elements.length ; i++) {
 		_elements[i].addEventListener('click', ElementClicked, false);
 	}
