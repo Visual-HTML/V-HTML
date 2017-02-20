22af722f7f4040078e7c6984e313b8e74ccc3e0e
Visual-HTML <visualhtml@gmail.com>
2017-02-20 22:53:15 +0100

    Editor Wrapper Code (removed panels)
    * focus at wrapping elements 
    * focus on editable element
    
    - let begin in default contentEditable on body
    - once one wrapping has been done, completely handle edits while keeping contentEditable on focused element
    
    IE don't allow me this wrapper (by custom tag-it create new elements on previous one) logic, using expando on elements will involve query by processing all nodes
    here with a custom (which is also expando) attribute I can get the full power of document.body.querySelectorAll('body *:not([data-VH2017-hndk])'); , this will reduce my set to process, thus preserving performance.
    
    So at this time:
    ---begin with body editable-------clean, blank, quick...
    ---then after the first wrap, body is removed from contentEditable and I switch to a custom edit mode 
    
    -----------At editor level: click is sent to consumer code, 
    ---------------------------focusout is only encapsulation purpose : it manage the contentEditable attribute
    --------------------------this click event code manage  the contentEditable attribute, the VH2017.currentTarget and the click callback

--- VH2017.js
+++ VH2017.js
@@ -1,55 +1,134 @@
 window.addEventListener('load', InitializeUserAgent, true);
-window.addEventListener('load', InitializeDocument, true);
-window.addEventListener('load', InitializeContent, true);
+
+
 
 VH2017 = {};
 VH2017.document={};
 VH2017.document.body={};
 VH2017.document.body.contentEditable={};
 VH2017.document.body.designMode={};
-
-function InitializeUserAgent(evt) {
+VH2017.currentTarget = null;
+VH2017.Clear = function() { 
+	document.body.removeAttribute("contentEditable");
+	document.removeEventListener('keydown', DocumentKeyDown, false);	
+	document.removeEventListener('click', DocumentClick, false);
 	
+    var _elements = document.querySelectorAll('*');
+	for (var i = 0 ; i < _elements.length ; i++) {
+		_elements[i].removeEventListener('click', ElementClick, false);
+		_elements[i].removeAttribute("data-VH2017-hndk");
+	}
+	
+};
+VH2017.ElementClick = function(evt) { };
+VH2017.ElementWrap = function(elt) { };
+
+
+
+function InitializeUserAgent(e) {
+	
+	InitializeDocument();
 }
 
-function InitializeDocument(evt) {
+function InitializeDocument() {
+	
 	VH2017.document.body.contentEditable.InitalValue = document.body.contentEditable;
 	VH2017.document.body.designMode.InitialValue = document.designMode;
+		
+	document.body.addEventListener('keydown', DocumentKeyDown, false);	
+	document.body.addEventListener('click', DocumentClick, false);
 	
-	/* if the page is set to design mode, VH don't go further */
-	if (VH2017.document.body.designMode.InitialValue.toLowerCase === "Inherit") return;
-	/* 
-	this mode allow so much that we will use it to open a blank sheet, user will be able to work 
-	in that mode and get the code injected to the VH page
-	*/
 	
-	/* contentEditable processing : in investigation */
+	document.body.contentEditable = true;
 	
-	var xReq = new XMLHttpRequest();
-	xReq.open("GET", "https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/Sources/testmain.html", true); 
-	xReq.timeout = 2000;
-	xReq.ontimeout = function () {   } 
-	xReq.onreadystatechange = function (e) {
-		if (xReq.readyState == 4) {         
-			if (xReq.status = "200") { 
-				document.body.innerHTML += xReq.response;
-				} else {
-				
-				}
-			}
-		}
-	xReq.send(null);
+	VH2017.currentTarget = document.body;
+	VH2017.currentTarget.focus();
+
+	InitializeContent();
+	
 }
 
-function InitializeContent(evt) {
-    _elements = document.querySelectorAll('*');
+
+
+function InitializeContent() {	WrapElements(); }
+
+
+
+function ElementClick(e) {
+	
+    e.stopPropagation();
+	
+	VH2017.currentTarget = e.currentTarget;
+	VH2017.currentTarget.contentEditable = true;
+	VH2017.currentTarget.focus();
+	
+	VH2017.ElementClick(e);
+
+}
+
+
+
+function ElementFocusOut(e) {
+	
+	e.currentTarget.removeAttribute("contentEditable");
+
+}
+
+
+
+function DocumentClick(e) {
+	
+	VH2017.currentTarget = e.currentTarget;
+	VH2017.currentTarget.focus();
+}
+
+
+
+function DocumentKeyDown(e) { 
+   if (e.which === 13) {
+	   
+	   if (document.body.hasAttribute("contentEditable")) {
+	       WrapElements(); 
+	
+		} else {
+			e.preventDefault();
+			
+			var _element = document.createElement("p");
+			WrapElements(document.body.appendChild(_element));
+		}
+   }
+}
+
+
+
+function WrapElements(elt) {
+	
+	if (typeof(elt) === "undefined") {
+		
+	// look for and attach unhandled elements
+	var _elements = document.body.querySelectorAll('body *:not([data-VH2017-hndk])');
+	
 	for (var i = 0 ; i < _elements.length ; i++) {
-		_elements[i].addEventListener('click', ElementClicked, false);
+	  if ( _elements[i].nodeType === 1 
+					 && !_elements[i].hasAttribute("data-VH2017-dsgk")
+					 && !_elements[i].hasAttribute('data-VH2017-hndk') ) { 
+		_elements[i].addEventListener('focusout', ElementFocusOut, false); 
+		_elements[i].addEventListener('click', ElementClick, false);
+		_elements[i].setAttribute("data-VH2017-hndk","");
+		VH2017.ElementWrap(_elements[i]);
+	  }
+	  document.body.removeAttribute("contentEditable");
+	}
+	
+	
+	} else {
+		elt.addEventListener('click', ElementClick, false);
+		elt.addEventListener('focusout', ElementFocusOut, false); 
+		elt.setAttribute("data-VH2017-hndk","");
+		VH2017.ElementWrap(elt);
+		elt.contentEditable = true;
+		elt.focus();
 	}
 }
 
-function ElementClicked(e) {
-  e.stopPropagation();
-  e.preventDefault();
-  // a handle on current element is available in e.currentTarget
-}
+
