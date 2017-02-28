a1da696894382a92d5c6b1c0aeb0438aa337bdd6
Visual-HTML <visualhtml@gmail.com>
2017-02-28 22:13:23 +0100

    Create 20170228_0.js

---
+++ todel/20170228_0.js
@@ -0,0 +1,3 @@
+VH2017.AddResource("https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170228_1.html");
+foo();
+VH2017.RemoveResource("https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170228_1.html");
