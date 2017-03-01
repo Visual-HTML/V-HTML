a355302e0df1873244fcf1f3654668f1d6b661d2
Visual-HTML <visualhtml@gmail.com>
2017-03-01 01:14:10 +0100

    Update 20170228_1.js

--- todel/20170228_1.js
+++ todel/20170228_1.js
@@ -1,5 +1,1 @@
-(function(){
-  VH2017.AddResource("https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170228_1.html"); 
-  foo(); 
-  VH2017.RemoveResource("https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170228_1.html");
-})();
+function fa(){ alert(""); };
