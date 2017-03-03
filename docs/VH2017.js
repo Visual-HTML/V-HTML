d177cfd65e77003d3def40268459d525ccb9bb0f
Visual-HTML <visualhtml@gmail.com>
2017-03-03 21:15:16 +0100

    Start View/edit Mode
    - introduce editor's toolbar (same element id added/removed by current processing)
    - view mode : minimal code needed to load editor
    - a document saved in view mode is re-open in view mode
    - a document saved in edit mode is re-open in edit mode

--- docs/VH2017.js
+++ docs/VH2017.js
@@ -1,16 +1,18 @@
-// when page is loaded, start initialization process: set user-agent specific code
+/* when page is loaded, start initialization process: set user-agent specific code */
 window.addEventListener('load', InitializeUserAgent, false);
 
-// isolate editors code within VH2017, which become is an Object in window
+/* isolate editors code within VH2017, which become is an Object in window */
 VH2017 = {};
 VH2017.document={};
 VH2017.document.body={};
 VH2017.document.body.contentEditable={};
 VH2017.document.body.designMode={};
 VH2017.CurrentTarget = null;
-VH2017.DesignerUrl = "https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170226.html";
+VH2017.DesignerUrl = null;
+//"https://raw.githubusercontent.com/Visual-HTML/V-HTML/master/todel/20170226.html"
 VH2017._TmpElt = null;
 VH2017.LoadDesignerScript = function() {
+	/*
 	// First version, its a script fragment, poc'n designer
 	
 	// Designer at this time is loaded at start but I must be able to switch between designer on the same authored document
@@ -40,6 +42,7 @@ VH2017.LoadDesignerScript = function() {
 	}
 	xReq.send(null);
 	*/
+	if (this.DesignerUrl == null || this.DesignerUrl.replace(" /g","") == "" ) return;
 	
 	 var xReq = new XMLHttpRequest();
 	 xReq.open("GET", this.DesignerUrl, false);
@@ -73,14 +76,14 @@ VH2017.IncludeDynamicScript = function(url) {
 	var _elt2 = document.createElement("script");
 	/*_elt2.setAttribute("data-VH2017-Res", url);*/
 	_elt2.src =  url;
-	//_elt2.type="application/javascript";
+	/*_elt2.type="application/javascript";*/
 	/*_elt2.language="javascript";*/
 	 //document.head.appendChild(_elt2);
 	 document.body.insertBefore(_elt2,document.body.firstChild);
-	 // Scripts can be added for different purpose,
-	 //VH2017.CurrentTarget.appendChild(_elt2);
-	 //document.head.appendChild(_elt2);
-	 // All these functions : AddResource, IncludeDynamicScript, RemoveResource, RemoveDynamicScript
+	 /* Scripts can be added for different purpose,
+	 VH2017.CurrentTarget.appendChild(_elt2);
+	 document.head.appendChild(_elt2);
+	  All these functions : AddResource, IncludeDynamicScript, RemoveResource, RemoveDynamicScript
 	 
 	 // will allow designer, starters and custom controls to load/unload code from page with single script reference (url)
 	 // suppose to understand why : I have a designer for bootstrap and use it to get a page grid built on their code
@@ -94,6 +97,7 @@ VH2017.IncludeDynamicScript = function(url) {
 	
 	// IT'S not validated nor strong but for sure it is POC'ed, to note this difference in behavior when code is injected (AddResource)
 	// or included using a script source (IncludeDynamicScript)
+	*/
 };
 VH2017.RemoveResource = function(url) {
 	
@@ -108,20 +112,28 @@ VH2017.RemoveDynamicScript = function(url) {
 	 
 };
 VH2017.LoadDesignerCSS = function() {
-	// First version, its a css fragment, poc'n designer
+	/*
+	 First version, its a css fragment, poc'n designer
 		
 	// Styles also are designer dependent, what editor can do is to add the designer's styles just after the script node
-	// Here a function such as VH2017.head.AddDesignerStyles(code)---why do I need to manage this, to clean the code, to provide basic enable/disable...
+	// Here a function such as VH2017.head.AddDesignerStyles(code)---why do I need to manage this, to clean the code, to provide basic enable-disable...
 	// Add designer' styles
+	*/
+	
 	var  _element = document.createElement("style");
 	_element.title = "VH2017- Designer Styles";
 	_element.id = "VH2017-Designer-Styles";
 	_element.innerHTML += "*:not(hr)[data-VH2017-hndk] { min-height: 20px; border: 1px dotted gray; } ";	
-	// Designer styles are added just after this script link
+	//_element.innerHTML += "body { margin-top: 100px; border-top: 1px solid gray; } ";	
+	_element.innerHTML += "body { border-top: 1px solid gray; } ";	
+	//_element.innerHTML += "#Designer-Toolbar { position: absolute; top: 0px; } ";	
+	/* Designer styles are added just after this script link */
 	var _aux = document.head.querySelectorAll('script');
 	var _aux1 = document.head.querySelector('script[src$="VH2017.js"]');
-	 _aux1.parentNode.insertBefore(_element, _aux1.nextElementSibling);
+	_aux1.parentNode.insertBefore(_element, _aux1.nextElementSibling);
 	 
+	 
+	if (this._TmpElt == null) return;	
 	 var _overridestyle = document.getElementById("VH2017-Designer-Styles");
 	 if ( this._TmpElt.getElementsByTagName('style').length > 0)
 	       _overridestyle.innerHTML = this._TmpElt.getElementsByTagName('style')[0].innerHTML;
@@ -129,8 +141,38 @@ VH2017.LoadDesignerCSS = function() {
 
 };
 VH2017.LoadDesignerHTML = function() {
-	// First version, I had not html in my current code, poc'n designer
-
+	/* First version, I had not html in my current code, poc'n designer */
+	
+	
+		if (document.body.querySelector('#Designer-Toolbar') != null) {
+	try { document.body.querySelector('#Designer-Toolbar').remove(true); console.log("used:.remove(true)"); } 
+	catch(xcp) {		
+		try {
+		document.body.querySelector('#Designer-Toolbar').removeNode(true); console.log("used:.removeNode(true)"); 
+		} catch(xcp) { 
+		document.body.removeChild(document.body.querySelector('#Designer-Toolbar')); console.log("used:.removeChild(elt)"); }
+		finally { console.log("cross-browser"); };		
+	} 
+	finally { console.log("cross-browser"); };
+		}
+	
+	
+	var _defaultDesignerToolbar;
+	
+		_defaultDesignerToolbar = document.createElement("div");
+		_defaultDesignerToolbar.id = "Designer-Toolbar";
+	
+	
+	var _clearButton = document.createElement("input");
+	_clearButton.type = "button";
+	_clearButton.value = "Clear";
+	_clearButton.addEventListener("click", function(e){ e.stopPropagation(); VH2017.Clear(); }, false);
+	
+	_defaultDesignerToolbar.appendChild(_clearButton);	
+	
+	
+		document.body.insertBefore(_defaultDesignerToolbar, document.body.firstChild);
+	
 };
 VH2017.Clear = function() {
 	
@@ -145,7 +187,7 @@ VH2017.Clear = function() {
 	
 	_elements = document.body.querySelectorAll("body *[contentEditable='false']");
 	for (var i = 0 ; i < _elements.length ; i++) {
-		try { _elements[i].xremove(true);  console.log("used:.remove(true)"); } 
+		try { _elements[i].remove(true);  console.log("used:.remove(true)"); } 
 		catch(xcp) { 
 			
 			try {
@@ -158,7 +200,7 @@ VH2017.Clear = function() {
 		finally { console.log("cross-browser"); };
 	}
 	
-	try { document.head.querySelector('#VH2017-Designer-Styles').xremove(true); console.log("used:.remove(true)"); } 
+	try { document.head.querySelector('#VH2017-Designer-Styles').remove(true); console.log("used:.remove(true)"); } 
 	catch(xcp) {		
 		try {
 		document.head.querySelector('#VH2017-Designer-Styles').removeNode(true); console.log("used:.removeNode(true)"); 
@@ -168,15 +210,65 @@ VH2017.Clear = function() {
 	} 
 	finally { console.log("cross-browser"); };
 	
+		
+	try { document.body.querySelector('#Designer-Toolbar').remove(true); console.log("used:.remove(true)"); } 
+	catch(xcp) {		
+		try {
+		document.body.querySelector('#Designer-Toolbar').removeNode(true); console.log("used:.removeNode(true)"); 
+		} catch(xcp) { 
+		document.body.removeChild(document.body.querySelector('#Designer-Toolbar')); console.log("used:.removeChild(elt)"); }
+		finally { console.log("cross-browser"); };		
+	} 
+	finally { console.log("cross-browser"); };
+	
+	
+	
+	try { document.head.querySelector('script[src$="VH2017.js"]').remove(true); console.log("used:.remove(true)"); } 
+	catch(xcp) {		
+		try {
+		document.head.querySelector('script[src$="VH2017.js"]').removeNode(true); console.log("used:.removeNode(true)"); 
+		} catch(xcp) { 
+		document.body.removeChild(document.head.querySelector('script[src$="VH2017.js"]')); console.log("used:.removeChild(elt)"); }
+		finally { console.log("cross-browser"); };		
+	} 
+	finally { console.log("cross-browser"); };
+	
+		
+	
+	//Add Get Editor function
+	var _backeditorHTML = document.createElement("div");
+	_backeditorHTML.id = "Designer-Toolbar";
+	var _inputButton = document.createElement("input");
+	_inputButton.value = "get editor";
+	_inputButton.setAttribute("value", "get editor");
+	_inputButton.type = "button";
+	_inputButton.setAttribute("onclick", "Remove(); GetBackEditor(); ");
+	var _backeditor = document.createElement("script");
+	_backeditor.innerHTML += "function GetBackEditor() {";
+	_backeditor.innerHTML += "var _elt = document.createElement('script'); ";
+	_backeditor.innerHTML += "_elt.src = 'VH2017.js'; ";
+	_backeditor.innerHTML += "_elt.onload = function() { InitializeUserAgent(); }; ";
+	_backeditor.innerHTML += "document.head.appendChild(_elt); ";
+	//_backeditor.innerHTML += "InitializeUserAgent(); ";
+	_backeditor.innerHTML += "}; ";
+	_backeditor.innerHTML += "function Remove() {";
+	_backeditor.innerHTML += "try { document.body.querySelector('#Designer-Toolbar').remove(true); console.log('used:.remove(true)'); } catch(xcp) { try { 		document.body.querySelector('#Designer-Toolbar').removeNode(true); console.log('used:.removeNode(true)'); } catch(xcp) { 		document.body.removeChild(document.body.querySelector('#Designer-Toolbar')); console.log('used:.removeChild(elt)'); } finally { console.log('cross-browser'); }; } finally { console.log('cross-browser'); }; ";
+	_backeditor.innerHTML += "}; ";
+	 
+	_backeditorHTML.appendChild(_inputButton);
+	_backeditorHTML.appendChild(_backeditor);
+		
+	document.body.insertBefore(_backeditorHTML, document.body.firstChild);
+	
 };
 VH2017.WrapElement = function(elt) {
-	// Initialize element with required events and attributes
+	/* Initialize element with required events and attributes */ 
 	elt.addEventListener('keydown', ElementKeyDown, false);
 	elt.addEventListener('click', ElementClick, false);
 	elt.setAttribute("data-VH2017-hndk","");
 }
 VH2017.UnWrapElement = function(elt) {
-	// Remove from HTMLElement required events and attributes
+	/* Remove from HTMLElement required events and attributes */
 	elt.removeEventListener('keydown', ElementKeyDown, false);
 	elt.removeEventListener('click', ElementClick, false);
 	elt.removeAttribute("data-VH2017-hndk","");
@@ -189,23 +281,19 @@ VH2017.ElementWrap = function(elt) { };
 
 
 function InitializeUserAgent(e) {
+	/*
 	// jQuery remain the best solution to solve user-agent specific code but I'm trying to avoid using it at start
 	// Custom Controls and starters can use it but at the editor level I wish to implement a kind of dynamic loading
-	
+	*/ 
 	
 	VH2017.document.body.contentEditable.InitalValue = document.body.contentEditable;
 	VH2017.document.body.designMode.InitialValue = document.designMode;
 
 	document.body.removeAttribute("contentEditable");
 	document.designMode = "off";
-	// by time to time (when cache is updated?) I get an error adding events on document : document undefined ?
+	/* by time to time (when cache is updated?) I get an error adding events on document : document undefined ? */
 
-
-	VH2017.LoadDesignerScript();
-	
  
- 	VH2017.LoadDesignerCSS();
-	
 
 	InitializeDocument();
 	
@@ -219,7 +307,7 @@ function InitializeDocument() {
 	document.body.addEventListener('click', DocumentClick, false);	
 
 
-	VH2017.DesignerInitializeDocument();
+	if (typeof(VH2017.DesignerInitializeDocument) !== "undefined" ) VH2017.DesignerInitializeDocument();
 	
 	
 	InitializeContent();
@@ -231,20 +319,29 @@ function InitializeDocument() {
 
 function InitializeContent() {	
 
-	// Begin with a procces that wrap existing content
+	/* Begin with a procces that wrap existing content */
 	WrapElements();
 	
-    	// first child not for designer purpose get focused
+    	/* first child not for designer purpose get focused */
 	VH2017.CurrentTarget = document.body.querySelector("[contentEditable='true']");
 	VH2017.CurrentTarget.focus();
 	try { 
-	// some browser need to trigger a click after .focus()
+	/* some browser need to trigger a click after .focus() */
 	VH2017.CurrentTarget.click(); }
 	catch(xcp) { 
-	// while some other will not even provide the function ? it's maybe an element without click ? to check!
+	/* while some other will not even provide the function ? it's maybe an element without click ? to check! */
 	console.log("catch exception : .click() on "+VH2017.CurrentTarget.nodeName);  }
 	finally { };
 	
+	
+	//The following is designer purpose code, placing this initializarion (of the designer toolbar)	
+	//here make the document content wrapped and avoid making designer content wrapped...
+ 	VH2017.LoadDesignerCSS();	
+	
+ 	VH2017.LoadDesignerHTML();	
+	// loading script on the end make all css and html available to the script 
+	VH2017.LoadDesignerScript();
+		
 }
 
 
@@ -254,26 +351,30 @@ function ElementKeyDown(e) {
 	console.log( e.type + " currentTarget:" + e.currentTarget.nodeName + " activeElement:" +
 		(document.activeElement.nodeName ? document.activeElement.nodeName : null));
 
-	// All the following seem to be designer code!
+	/* All the following seem to be designer code! */ 
 
 	if (e.which === 13 && e.shiftKey) {	
+	/*
 	// this allow to prevent defaults for what I override, and not for a backspace , delete...
 	// This is a code related to shift+enter handling
+	*/
 	   	e.preventDefault();
 		e.stopPropagation();
  
-		
+		/*
 		// shift+enter must insert a br element at the current cursor position 
 		// https://www.w3.org/TR/html/single-page.html#the-br-element
+		*/
 		var _elt = document.createElement("br"); 
 		_elt.id = Date.now();
 
                 if ( typeof(document.getSelection().focusNode.data) !== "undefined") {
 			var _pos= e.currentTarget.innerHTML.indexOf(document.getSelection().focusNode.data);
 			_pos = (_pos === -1 ? 0 : _pos);
+			/*
 			// using getSelection may need index re-compute: result in _pos
 			// document.getSelection().focusOffset only give me the cursor position within a node
-			
+			*/
 			e.currentTarget.innerHTML = 
 				e.currentTarget.innerHTML.substring(0, _pos + document.getSelection().focusOffset)
 				+ _elt.outerHTML
@@ -284,20 +385,20 @@ function ElementKeyDown(e) {
 			e.currentTarget.innerHTML += _tmp;			
 		}
 		
-		WrapElementById(_elt.id); // This process with its id
-		//e.currentTarget.focus();
+		WrapElementById(_elt.id); /* This process with its id */
+		/*e.currentTarget.focus();*/
 		
 	}
 		
 	if (e.which === 13 && !e.shiftKey) {
-		// prevent default only in this case: return down, not even released
+		/* prevent default only in this case: return down, not even released */
 	   	e.preventDefault();
 		e.stopPropagation();
 
-		// Add content pressing enter
+		/* Add content pressing enter */
 		var _elt = document.createElement("p");			
 		var _res = e.currentTarget.parentNode.insertBefore(_elt, e.currentTarget.nextElementSibling);
-		// and wrap it
+		/* and wrap it */
 		WrapElements(_res);
 	}
 	   
@@ -314,7 +415,7 @@ function ElementClick(e) {
 	e.preventDefault();
 	
 	VH2017.CurrentTarget = e.currentTarget;	
-	VH2017.ElementClick(e);  // Inform designer that element was selected
+	VH2017.ElementClick(e);  /* Inform designer that element was selected */
 
 }
 
@@ -325,7 +426,6 @@ function DocumentClick(e) {
 	console.log( e.type + " currentTarget:" + e.currentTarget.nodeName + " activeElement:" +
 		(document.activeElement.nodeName ? document.activeElement.nodeName : null));
 	
-	fa();
 	
 }
 
@@ -336,24 +436,24 @@ function DocumentKeyDown(e) {
 	console.log( e.type + " currentTarget:" + e.currentTarget.nodeName + " activeElement:" +
 		(document.activeElement.nodeName ? document.activeElement.nodeName : null));
 	
-	// This is designer code!
+	/* This is designer code! */
 	if (e.which === 13) {
 		// Enter on document add a new paragraph
 		e.preventDefault();
 		
-		var _res = document.body.appendChild(document.createElement("p")); //TODO: designer must be able to say what element is added : div ? ul, ol? blockquote?...
+		var _res = document.body.appendChild(document.createElement("p")); /*TODO: designer must be able to say what element is added : div, ul, ol? blockquote?...*/
 		WrapElements(_res);
 	}
 	
 }
 
 
-// Wrapping elements allow to get a handle on them interactivity with a click
+/* Wrapping elements allow to get a handle on them interactivity with a click */
 function WrapElements(elt) {
-	// This is called without parameter, elt argument is undefined : the process apply on...
+	/* This is called without parameter, elt argument is undefined : the process apply on... */
 	if (typeof(elt) === "undefined") {
 			
-		// look for and attach unhandled elements ...apply on all elements not already handled and elements not set as not editable
+		/* look for and attach unhandled elements ...apply on all elements not already handled and elements not set as not editable */
 		var _elements = document.body.querySelectorAll("body *:not([data-VH2017-hndk]):not([contentEditable='false'])");
 		
 		for (var i = 0 ; i < _elements.length ; i++) {
@@ -363,12 +463,12 @@ function WrapElements(elt) {
 					
 				VH2017.WrapElement(_elements[i]);
 				
-				// This try to set contentEditable only on elements that user see as to edit (//TODO: in dev, need re-work)
+				/* This try to set contentEditable only on elements that user see as to edit (//TODO: in dev, need re-work) */
 				if ((_elements[i].childNodes.length === 1 && _elements[i].childNodes[0].nodeName === "#text")
 						|| (_elements[i].childNodes.length > 1 && _elements[i].childNodes[0].textContent != "\n  ") ) 
 					_elements[i].contentEditable = true;
 					
-				// Send info to handler	
+				/* Send info to handler */ 
 				VH2017.ElementWrap(_elements[i]);
 				}
 		}
@@ -377,11 +477,11 @@ function WrapElements(elt) {
 		
 		VH2017.WrapElement(elt);
 		
-		// This is already a designer option, a designer can choose to provide editing on text otherwise than this function 
+		/* This is already a designer option, a designer can choose to provide editing on text otherwise than this function */
 		elt.contentEditable = true;
 		
 		VH2017.CurrentTarget = elt;
-		VH2017.ElementWrap(elt);  // Inform using handler
+		VH2017.ElementWrap(elt);  /* Inform using handler */
 		VH2017.CurrentTarget.focus();
 		
 	}
@@ -391,25 +491,30 @@ function WrapElements(elt) {
 
 
 function WrapElementById(id) {
+	/*
 	// This is the wrapper code for a temporarily identified element, 
 	// created by code generator these elements get an id only
 	// to enable this code to do a getElementById...
+	*/
 	var elt = document.getElementById(id);
-	
+	/*
 	// This is already a designer option, a designer can choose to provide editing on text otherwise than this function 
 	//elt.contentEditable = true;
 	
 	// id can be removed
+	*/
 	elt.removeAttribute('id');
 	
 	VH2017.WrapElement(elt);
 	VH2017.CurrentTarget = elt;
-	//this is what is done, call removed : VH2017.ElementWrap(elt);  // Inform using handler
+	/*this is what is done, call removed : VH2017.ElementWrap(elt);  --------- Inform using handler */
 	VH2017.CurrentTarget.focus();
-	
+	/*
 	// This function is used (and was created by a need of, rather, a designer code, so the editor provide
 	// this service while a designer, starter or custom control canmanage this (wrap a new injected element) its own way...
+	*/
 				  
 }
+
 
 
