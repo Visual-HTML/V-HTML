bfbeeb7e7d06ce46d26a86f1af4829c5ed4c2add
Visual-HTML <visualhtml@gmail.com>
2017-03-06 02:20:57 +0100

    Save As for IE
    This code revives the distributed codes and dynamic loading on multiple locations
    //TODO: cross-view in cases between absolute and relative urls, CORS 
    Question: how this piece of code will get back near the document: using onerror attribute ? it seems cool but I think, once again that editor should 'compile' a single file to allow local edit (just as save as to mht, think and redefine them in the document while keeping a link with online codes : UC: edit local, change of designer (online), ... save as save all changes, ? 
    
    //TODO: rework references and dynamic loading in a moving/distributed codebase ? review html codebase, base url.

---
+++ docs/VH2017-MSIE10.js
@@ -0,0 +1,13 @@
+VH2017.SaveAs = function(file) {
+	
+	// this is added to make it work but clear isn't defined as a save as pre-condition
+	VH2017.Clear();
+	// It fail a re-open because a dynamic code is re-executed before VH2017 init... 
+  
+	window.navigator.msSaveBlob(
+		new Blob(["<!DOCTYPE html>"
+			  +window.document.head.outerHTML
+			  +window.document.body.outerHTML
+			  + "</html>" ]), file);
+	
+}
