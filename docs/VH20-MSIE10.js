4c4797495431b3c8ad4e83bd586df12d07a07418
Visual-HTML <visualhtml@gmail.com>
2017-06-03 15:03:14 +0200

    Update VH20-MSIE10.js

--- docs/VH20-MSIE10.js
+++ docs/VH20-MSIE10.js
@@ -1,18 +1,2 @@
 
-VH20.CrossBrowser.SaveAs = function(file) {
-	
-	// this is added to make it work but clear isn't defined as a save as pre-condition
-	VH20.Clear();
-	// It fail a re-open because a dynamic code is re-executed before VH2017 init... 
-  
-	window.navigator.msSaveBlob(
-		new Blob(["<!DOCTYPE html>"
-			  +window.document.head.outerHTML
-			  +window.document.body.outerHTML
-			  + "</html>" ]), file);
-	
-}
-
-
-
-VH20.CrossBrowser.RemoveElement = function(elt) { elt.parentNode.removeChild(elt); };
+VH20.RemoveElement = function(elt) { elt.parentNode.removeChild(elt); };
