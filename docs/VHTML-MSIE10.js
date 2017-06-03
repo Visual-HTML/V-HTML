190c0758bfb4b62a988a0a0d0004b947d0ec8b7d
Visual-HTML <visualhtml@gmail.com>
2017-06-03 15:04:35 +0200

    Update VHTML-MSIE10.js

--- docs/VHTML-MSIE10.js
+++ docs/VHTML-MSIE10.js
@@ -2,7 +2,7 @@
 VHTML.document.SaveAs = function(file) {
 	
 	// this is added to make it work but clear isn't defined as a save as pre-condition
-	VH20.Clear();
+	VHTML.Clear();
 	// It fail a re-open because a dynamic code is re-executed before VH2017 init... 
   
 	window.navigator.msSaveBlob(
