830055801a1b37fee8aa848eb72f25ebe7007600
Visual-HTML <visualhtml@gmail.com>
2017-06-13 20:36:54 +0200

    Create KSW3CSSDT.js

---
+++ docs/20170611/KSW3CSSDT.js
@@ -0,0 +1,11 @@
+// This is my designer code for KSW3CSSDT.html
+
+//The first thing is to ensure VH20 is included
+
+// Setup the intialization code
+
+// Make the intialization run
+
+
+////////////////////// Other codes introduced byt the designer
+
