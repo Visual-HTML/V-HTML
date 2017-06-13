49e646ed8f37c5abc879bf47249db69c6dd509b4
Visual-HTML <visualhtml@gmail.com>
2017-06-13 21:51:22 +0200

    Create KSW3CSSDT.js

--- docs/20170611/KSW3CSSDT.js
+++ docs/20170611/KSW3CSSDT.js
@@ -2,27 +2,19 @@
 
 //The first thing is to ensure VH20 is included
 if (document.head.querySelector('script[src*="vh20.js"]') === null) {
+  
   var _elt = document.createElement("script");
   _elt.src = "VH20.js";
   var _currentscriptnode = document.head.querySelector('script[src*="KSW3CSSDT.js"]');
-  
-  _elt.onload = function() {
-    
-    
-    VH20.onInitializeUserAgent = function(){ alert("and now ?...my designer code");}
-    
+  _elt.onload = function() { 
+    // Act at VH20 Intialization process
+    VH20.onInitializeUserAgent = function(){ alert("and now ?...my designer code");}    
            
   }
-  
-  
   document.head.appendChild(_elt, _currentscriptnode.nextElementSibling);  
-
-
-
-
 }
 
-// Setup the intialization code
+// Setup code - code that don't act in the Intitialization process of VH20
 
 
 // Make the intialization run
