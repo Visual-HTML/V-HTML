e11fdf592c15926fbf7614a2f30016771d1c77ae
Visual-HTML <visualhtml@gmail.com>
2017-06-13 21:00:31 +0200

    Create KSW3CSSDT.js

--- docs/20170611/KSW3CSSDT.js
+++ docs/20170611/KSW3CSSDT.js
@@ -8,7 +8,7 @@ if (document.head.querySelector('script[src*="vh20.js"]') === null) {
   
   _elt.onload = function() {
     VH20.InitializeUserAgent = function(){ alert("and now ?...");}
-    
+    VH20.InitializeUserAgent();
     
   }
   
