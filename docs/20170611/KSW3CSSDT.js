f431c41ecf680f3d06b462c7d8489ca47e722571
Visual-HTML <visualhtml@gmail.com>
2017-06-13 20:48:28 +0200

    Create KSW3CSSDT.js

--- docs/20170611/KSW3CSSDT.js
+++ docs/20170611/KSW3CSSDT.js
@@ -1,6 +1,12 @@
 // This is my designer code for KSW3CSSDT.html
 
 //The first thing is to ensure VH20 is included
+if document.head.querySelector('script[src*="vh20.js"]').length == 0 {
+  var _elt = document.createElement("script");
+  _elt.src = "VH20.js";
+  var _currentscriptnode = document.head.querySelector('script[src*="KSW3CSSDT.js"]');
+  document.head.appendChild(_elt, _currentscriptnode.nextElementSibling);  
+}
 
 // Setup the intialization code
 
