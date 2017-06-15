fde4ab52d1cb5e147dd4fd00e00c3206ad24c66f
Visual-HTML <visualhtml@gmail.com>
2017-06-15 20:25:45 +0200

    Create VH20.js

--- docs/VH20.js
+++ docs/VH20.js
@@ -76,7 +76,8 @@ VH20.LoadDesignerCSS = function() {
 	_element.innerHTML += "*:not(hr)[data-VH20-hndk] { min-height: 20px; border: 1px dotted gray; } ";	
 	//_element.innerHTML += "body { margin-top: 100px; border-top: 1px solid gray; } ";	
 	_element.innerHTML += "body { border-top: 1px dotted lightgray; padding-top: 100px; } ";	
-	_element.innerHTML += "#Designer-Toolbar { position: fixed; top: 0px; border-bottom: 1px dotted lightgray; } ";	
+	_element.innerHTML += "#Designer-Toolbar { position: fixed; top: 0px; border-bottom: 1px dotted lightgray; } ";		
+	_element.innerHTML += "#Designer-Toolbar span { font-size: xx-small; } ";	
 	/* Designer styles are added just after this script link */
 	var _aux = document.head.querySelectorAll('script');
 	var _aux1 = document.head.querySelector('script[src*="VH20.js"]');
