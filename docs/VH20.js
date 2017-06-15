1bf64d8cd3e9c95de00574632144c724fbda6d40
Visual-HTML <visualhtml@gmail.com>
2017-06-15 21:11:09 +0200

    Create VH20.js

--- docs/VH20.js
+++ docs/VH20.js
@@ -198,7 +198,7 @@ VH20.Clear = function() {
 	_backeditor.innerHTML += "_elt.src = '" + _currentscriptsrc + "'; ";
 	_backeditor.innerHTML += "_elt.onload = function() { VH20.RemoveElement(document.body.querySelector('#Designer-Toolbar')); VH20.InitializeUserAgent(" + (this.DesignerUrl != null ? "'"+this.DesignerUrl+"'" : "") + "); }; ";
 	//_backeditor.innerHTML += "_elt.onerror = function() { window.open('https://github.com/Visual-HTML/V-HTML/wiki/Get-Editor-Code'); }; ";
-	_backeditor.innerHTML += "_elt.setAttribute('onerror', 'avascript:window.open(\"https://github.com/Visual-HTML/V-HTML/wiki/Get-Editor-Code\");");
+	_backeditor.innerHTML += "_elt.setAttribute('onerror', 'javascript:window.open(\"https://github.com/Visual-HTML/V-HTML/wiki/Get-Editor-Code\");');";
 	_backeditor.innerHTML += "document.head.insertBefore(_elt, document.head.firstChild); ";
 	_backeditor.innerHTML += "}; ";
 	 
