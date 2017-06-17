966f2af5201e704bdc47d3cea36adf3d9172647c
Visual-HTML <visualhtml@gmail.com>
2017-06-17 15:21:00 +0200

    Update VH20.js

--- docs/VH20.js
+++ docs/VH20.js
@@ -395,15 +395,16 @@ VH20.InitializeUserAgent = function(url) {
 	
 	if (url != null) {
 		VH20.DesignerUrl = url;
-	
-		// loading script on the end make all css and html available to the script 
-		VH20.LoadDesignerScript();  // algorithm must be changed to make them used in different order ?	
-		VH20.LoadDesignerCSS(); 		
-		VH20.LoadDesignerHTML();
-		// different order, different file structure : split on several documents, one single file...
-	}
+		VH20.SwitchDesigner(VH20.DesignerUrl);
+    	}
 	
 	VH20.InitializeContent();
+	
+	// loading script on the end make all css and html available to the script 
+	VH20.LoadDesignerScript();  // algorithm must be changed to make them used in different order ?	
+	VH20.LoadDesignerCSS(); 		
+	VH20.LoadDesignerHTML();
+	// different order, different file structure : split on several documents, one single file...
 	
 	//The following is designer purpose code, placing this initialization (of the designer toolbar)	
 	//here make the document content wrapped and avoid making designer content wrapped...	
