6993335fdce8370bb020c9cb77fcf38a99f2ef61
Visual-HTML <visualhtml@gmail.com>
2017-06-18 23:01:58 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -1,7 +1,13 @@
 
 VH20.OnWindowBeforeUnload = function() {	
  
-alert("A"); window.document.body.addEventListener("beforeunload", function(e) { e.preventDefault(); alert("A1"); });
+alert("B"); 
+ 
+window.addEventListener("beforeunload", function (event) {
+  event.returnValue = "my Coucara-salsa";
+alert("B"); 
+}); 
+ 
   
 } 
 
