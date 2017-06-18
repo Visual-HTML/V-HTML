33b1c9b8d81cb1a70611f0c948566ced09049ae8
Visual-HTML <visualhtml@gmail.com>
2017-06-18 20:50:13 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -1,12 +1,19 @@
 
 
 VH20.OnWindowBeforeUnload = function() {	
+ 
  alert("A");
+ 
  window.onbeforeunload = function () {
   //event.preventDefaults();
-  alert("A");
+  alert("A1");
   event.returnValue = "Document about to be unloaded, this allow to choose to proceed or remain on the document.";
   return "Document about to be unloaded, this allow to choose to proceed or remain on the document.";
 }
-
+ 
+window.addEventListener('beforeunload', function() {
+ alert("A2");
+ return "Document about to be unloaded, this allow to choose to proceed or remain on the document.";
+}, false);
+ 
 } 
