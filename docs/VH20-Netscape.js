78084320e9c3e78acdd4d50219779e2b85a5b051
Visual-HTML <visualhtml@gmail.com>
2017-06-18 23:48:52 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -1,10 +1,13 @@
 
 VH20.OnWindowBeforeUnload = function() {	
  
-alert("E"); 
- 
-//TODO: find the code to handle document unload warning - Safari' code 
-document.body.setAttribute("onbeforenload", "function(e){ return 'xxx'; }", true);
+alert("A"); 
+
+window.onbeforeunload = function () {   
+ //TODO: find the Safari' code to handle document unload warning
+  window.event.returnValue = false;   
+  return false;    
+} 
   
 } 
 
