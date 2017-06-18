61145a1d8d098137e8d2ef296399d57a630b3785
Visual-HTML <visualhtml@gmail.com>
2017-06-18 17:00:53 +0200

    Create VH20-Safari.js

---
+++ docs/VH20-Safari.js
@@ -0,0 +1,9 @@
+
+
+VH20.OnWindowBeforeUnload = function() {	
+ 
+ window.onbeforeunload = function () {
+  return "Document about to be unloaded, this allow to choose to proceed or remain on the document."
+}
+
+} 
