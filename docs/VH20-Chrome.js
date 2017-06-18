1bb310f70f0306c25c75f71a3541dfcba55aed60
Visual-HTML <visualhtml@gmail.com>
2017-06-18 19:43:38 +0200

    Update VH20-Chrome.js

--- docs/VH20-Chrome.js
+++ docs/VH20-Chrome.js
@@ -3,7 +3,8 @@
 VH20.OnWindowBeforeUnload = function() {	
  
 window.onbeforeunload = function() { 
- return "Document about to be unloaded, this allow to choose to proceed or remain on the document."; };
+ event.returnValue = "Document about to be unloaded, this allow to choose to proceed or remain on the document."; 
+};
  
 } 
 
