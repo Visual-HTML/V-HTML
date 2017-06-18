d52c503876cbb00a6f721b9b629b52c19e3b3ee3
Visual-HTML <visualhtml@gmail.com>
2017-06-18 23:23:32 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -4,7 +4,7 @@ VH20.OnWindowBeforeUnload = function() {
 alert("D"); 
  
 window.addEventListener("beforeunload", function (e) {
-  var confirmationMessage = "\o/";
+  var confirmationMessage = "D1";
 
   (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
   return confirmationMessage;                                //Webkit, Safari, Chrome etc.
