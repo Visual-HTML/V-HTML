bedc4b9644d2c67755b526d76626fe8bcf1f9b4e
Visual-HTML <visualhtml@gmail.com>
2017-06-18 17:05:09 +0200

    Update VH20-Safari.js

--- docs/VH20-Safari.js
+++ docs/VH20-Safari.js
@@ -3,6 +3,7 @@
 VH20.OnWindowBeforeUnload = function() {	
  
  window.onbeforeunload = function () {
+  event.returnValue = confirmationMessage;
   return "Document about to be unloaded, this allow to choose to proceed or remain on the document."
 }
 
