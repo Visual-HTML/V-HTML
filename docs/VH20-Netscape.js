e8a2f9532a9bbf23dd8b5fa51dbe1bdc634418e0
Visual-HTML <visualhtml@gmail.com>
2017-06-19 20:04:20 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -1,19 +1,18 @@
 
 VH20.OnWindowBeforeUnload = function() {	
- 
- alert(window.onunload);
- alert(window.onbeforeunload);
- alert(window.document.onunload);
- alert(window.document.onbeforeunload);
- alert(window.document.body.onunload);
- alert(window.document.body.onbeforeunload);
- 
+  
 window.onunload = function () {  
  alert("event caught ?");
  //TODO: find the Safari' code to handle document unload warning
   window.event.returnValue = false;   
   return false;    
 } 
-  
+
+
+ alert(window.onunload);
+ alert(window.onbeforeunload);
+ alert(window.document.body.onunload);
+ alert(window.document.body.onbeforeunload);
+
 } 
 
