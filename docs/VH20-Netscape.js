4c1e33a91492a3744612f00ba484fcf5dbbfb545
Visual-HTML <visualhtml@gmail.com>
2017-06-19 19:49:49 +0200

    Update VH20-Netscape.js

--- docs/VH20-Netscape.js
+++ docs/VH20-Netscape.js
@@ -1,6 +1,9 @@
 
 VH20.OnWindowBeforeUnload = function() {	
  
+ alert(window.onunload);
+ alert(window.onbeforeunload);
+ 
 window.onunload = function () {  
  alert("event caught ?");
  //TODO: find the Safari' code to handle document unload warning
