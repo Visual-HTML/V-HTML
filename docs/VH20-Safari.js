b898adbbf02be5d98145c1849fce446246d96b82
Visual-HTML <visualhtml@gmail.com>
2017-06-30 19:41:13 +0200

    Create VH20-Safari.js

---
+++ docs/VH20-Safari.js
@@ -0,0 +1,10 @@
+VH20.OnWindowBeforeUnload = function() {	
+  
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage = "\o/";
+
+  (e || window.event).returnValue = confirmationMessage;     //Gecko + IE
+  return confirmationMessage;                                //Webkit, Safari, Chrome etc.
+});
+  
+} 
