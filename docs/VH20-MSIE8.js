e53dcaa59c7c4d9a375d9e244ba84d032365159c
Visual-HTML <visualhtml@gmail.com>
2017-09-21 00:36:07 +0200

    Update VH20-MSIE8.js

--- docs/VH20-MSIE8.js
+++ docs/VH20-MSIE8.js
@@ -31,3 +31,33 @@ VH20.document.InitializeEvents = function() {
    document.body.attachEvent('onclick', VH20.DocumentClick);
 }
 	
+
+VH20.WrapDocument = function() { 
+	/* look for and attach unhandled elements ...apply on all elements not already handled and elements not set as not editable */
+	/* ! this function can be called on a document on which .Clear() has been runned */
+	var _elements = document.body.querySelectorAll("body *");
+	
+	for (var i = 0 ; i < _elements.length ; i++) {
+		if ( _elements[i].nodeType === 1 
+					 && !_elements[i].hasAttribute("data-VH20-dsge")
+					 && !_elements[i].hasAttribute("data-VH20-dsgk")
+					 && !_elements[i].hasAttribute('data-VH20-hndk') ) { 
+				
+			VH20.WrapElement(_elements[i]);
+			
+			/* This try to set contentEditable only on elements that user see as to edit (//TODO: in dev, need re-work) */
+			if ( (_elements[i].childNodes.length === 0) 
+					|| (_elements[i].childNodes.length === 1 && _elements[i].childNodes[0].nodeName === "#text")
+					|| (_elements[i].childNodes.length > 1 && _elements[i].childNodes[0].textContent != "\n  ") ) 
+				_elements[i].contentEditable = true;
+				
+			/* Send info to handler */ 
+			VH20.Events.ElementWrap(_elements[i]);
+			}
+	}
+	/* the whole wrapping logic must be reviewed to allow to : (within document content, provide information to wrapper logic)*/
+	/* 1. make an element part of the designer : at this time it is done setting it contentEditable=false, these elements disappear with .Clear()*/
+	/* 2. make an element part of the content, but not editable ! these elements remain as content and aren't editable */
+	/* often in the specification they say to use custom tag/expando to avoid collision, following this fact I should define custom tags */
+	/* for each cases here above and use contentEditable only for its specification purpose */
+}
