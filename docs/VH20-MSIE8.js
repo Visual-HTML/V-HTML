bae4fc19768ee8fbce4a3b7ee6b1b3c0e12dd64e
Visual-HTML <visualhtml@gmail.com>
2017-09-20 19:15:00 +0200

    Update VH20-MSIE8.js

--- docs/VH20-MSIE8.js
+++ docs/VH20-MSIE8.js
@@ -8,3 +8,14 @@ VH20.InitializeEvent = function() {
 }
 */
 
+
+
+VH20.OnWindowBeforeUnload = function() {
+	
+  // identified default code
+  window.attachEvent("onbeforeunload", function (e) {
+	  (e || window.event).returnValue = "Document about to be unloaded, this allow to choose to proceed or remain on the document.";     
+	  return "Document about to be unloaded, this allow to choose to proceed or remain on the document.";                                
+  });
+	
+} 
